a8f197fe205b39eb864af26c060f9d0e1e5a0242
Paulo Eduardo da Silva <pauloes.dev@gmail.com>
2025-04-27 18:19:59 -0300

    removing database cause we don't use it anymore

--- services/receipt-api/src/index.ts
+++ services/receipt-api/src/index.ts
@@ -4,7 +4,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
-import { initDb } from "./db/lowdb.client.js"; // Change the import to use .ts extension instead of .js
 import receiptRoutes from "./receipt/receipt.routes.js";
 
 const app = express();
@@ -22,9 +21,6 @@ app.use("/receipt", receiptRoutes);
 // Initialize the database before starting the server
 async function startServer() {
   try {
-    await initDb(); // Initialize the database
-    console.log("Database initialized successfully");
-
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
